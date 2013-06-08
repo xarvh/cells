c8103df82c90603975bd4c099ab7eb63414644dc
xarvh <francesco.orsenigo@adslot.com>
2013-06-08 11:52:03 +1000

    fix reaction calculation!

--- chemistry.py
+++ chemistry.py
@@ -55,8 +55,6 @@ class Chemistry:
     for i, r in enumerate(self.reactions): r.id = i
     for i, s in enumerate(self.species): s.id = i
 
-    ### check: all substances must appear at least once as products of a reaction
-    ### and at least once as reagents
 
 
   def react(self, quantities):
@@ -73,42 +71,49 @@ class Chemistry:
     by a certain reaction, the less the specie will be available for other
     reactions.
    
- 
     To calculate how to distribute a specie among all reactions, we compare
     the "demand" for that specie by a single reaction with the total demand for
     that specie.
     The demand by a specific reaction is defined as the maximum quantity that
     the reaction would consume if it was the only reaction present, times the
     reaction's speed.
-
-    The portion of a specie consumed by a certain reaction is weighted by the
-    specific demand diviided by the total demand.
     """
 
     reactions = self.reactions
     species = self.species
 
-    # raw demand for each reaction
-    # easier to calculate than specific demand
-    # raw_demand(r) := demand(r, s) / stechiometric_reagent_cf(r, s)
-    raw_demand = [ r.speed * min( q / cf for q, cf in zip(quantities, r.reagents) if cf ) for r in reactions ]
+    for s in species:
+      s.cfs = [ r.reagents[s.id] for r in reactions ]
 
+    # reactions speed
+    S = [ r.speed for r in reactions ]
 
-    # total demand for each specie
-    total_demand = [ sum( raw_demand[r.id] * r.reagents[s.id] for r in reactions ) for s in species ]
+    # max events per reaction
+    #
+    # Mr(Q) = min[s] Qs / Crs
+    #
+    M = [ min(Qs / Crs for Qs, Crs in zip(quantities, r.reagents) if Crs) for r in reactions]
 
-    # available quantity divided by total demand
-    weights = [ q / float(t) if t else 0 for q, t in zip(quantities, total_demand) ]
-    
+    # reaction power
+    #
+    # Pr = Sr Mr
+    #
+    P = [ Sr * Mr for Sr, Mr in zip(S, M)]
+
+    # demand for a specie
+    #
+    # Ds = sum[r] Pr Crs
+    #
+    D = [ sum(Pr * Crs for Pr, Crs in zip(P, s.cfs)) for s in species]
 
     # actual events per reaction
-    events = [
-      min( w * raw / cf for w, cf in zip(weights, r.reagents) if cf )
-      for raw in raw_demand]
-
+    #
+    # Er = Pr min[s] Qs / Ds [if Crs]
+    #
+    E = [int(Pr * min( Qs / float(Ds) if Ds else 0 for Qs, Ds, Crs in zip(quantities, D, r.reagents) if Crs)) for Pr, r in zip(P, reactions)]
 
     # remove used reagents
-    for e, r in zip(events, reactions):
+    for e, r in zip(E, reactions):
       for specie, cf_in, cf_out in zip(species, r.reagents, r.products):
         quantities[specie.id] += (cf_out - cf_in) * int(e)
 
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 from sys import argv
 from chemistry import Chemistry
 
-random.seed(0)
+#random.seed(0)
 
 chemfile = 'data/world.chem'
 if len(argv) > 2: chemfile = argv[2]
@@ -11,9 +11,11 @@ if len(argv) > 2: chemfile = argv[2]
 chem = Chemistry(open(chemfile).read())
 
 quantities = [random.randint(0, 100) for i in chem.species]
+print "start total:", sum(quantities)
 
 for i in range(int(argv[1])):
   print quantities
   quantities = chem.react(quantities)
 
 
+print "end total:", sum(quantities)
