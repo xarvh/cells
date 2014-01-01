a19daa73cecd319a99dc9e55ea3e740a1dd7c095
xarvh <francesco.orsenigo@adslot.com>
2014-01-01 15:19:05 +1100

    Allow appending frames

--- canvas/frames.js
+++ canvas/frames.js
@@ -1,10 +1,11 @@
 window.gridFrames = {
   width: 512,
   height: 512,
-  frames: [
-    {r: 255, g: 0, b: 0},
-    {r: 0, g: 255, b: 0},
-    {r: 0, g: 0, b: 255},
-  ]
-};
+  frames: []
+}
+var f = gridFrames.frames
+
+f.push({r: 255, g: 0, b: 0})
+f.push({r: 0, g: 255, b: 0})
+f.push({r: 0, g: 0, b: 255})
 
